51c41abdea55863326dffa5138e318ee3f179199
maestrovt <maestrovt@gmail.com>
2024-10-21 02:41:12 -0400

    Added Point and Line classes and draw method

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from window import Window
+from window import Window, Point, Line
 
 win = Window(800, 600)
+p1 = Point(100, 100)
+p2 = Point(200, 200)
+line1 = Line(p1, p2)
+win.draw_line(line1, "black")
 win.wait_for_close()
--- window.py
+++ window.py
@@ -9,6 +9,9 @@ class Window:
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+    
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
@@ -21,3 +24,16 @@ class Window:
 
     def close(self):
         self.__running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.__point1 = p1
+        self.__point2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.__point1.x, self.__point1.y, self.__point2.x, self.__point2.y, fill = fill_color, width = 2)

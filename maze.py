1f27c43378992a172b7a405de2157a351528796d
maestrovt <maestrovt@gmail.com>
2024-10-27 05:59:29 -0400

    added reset_cells_visited method

--- maze.py
+++ maze.py
@@ -29,6 +29,7 @@ class Maze:
         self._create_cells()
         self._break_entrance_and_exit()
         self._break_walls_r(0, 0)
+        self._reset_cells_visited()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -103,3 +104,8 @@ class Maze:
                 self._cells[i][j - 1].has_bottom_wall = False
 
             self._break_walls_r(new_i, new_j)
+
+    def _reset_cells_visited(self):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._cells[i][j].visited = False
--- tests.py
+++ tests.py
@@ -41,6 +41,17 @@ class Tests(unittest.TestCase):
             m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
             False
         )
+
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for i in range(num_cols):
+            for j in range(num_rows):
+                self.assertEqual(
+                    m1._cells[i][j].visited,
+                    False
+                )
     
 if __name__ == "__main__":
     unittest.main()
